83fd0ace24f17d49dd777c177f16b15d96b3f192
ahzahra <ahmed.zahra95@gmail.com>
2017-12-16 20:48:08 -0500

    working facial landmarks detection

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import numpy as np
 import cv2
+from facial_landmarks import *
+import matplotlib.pyplot as plt
 
-im1 = cv2.imread("obama.jpg")
-im2 = cv2.imread("trump.jpg")
+im1 = cv2.imread("obama.jpg", cv2.IMREAD_COLOR)
+im2 = cv2.imread("trump.jpg", cv2.IMREAD_COLOR)
 
-print im1.shape
+try:
+	landmarks = get_landmarks(im1)
+except NameError:
+	print "Error"
+
+plt.imshow(im1[..., ::-1])
+# plt.scatter(landmarks[:,0],landmarks[:,1])
+
+plt.show()
